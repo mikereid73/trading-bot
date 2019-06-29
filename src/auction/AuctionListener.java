bcc9ea353ee3df4cb159d44782c0378521c0d89a
Michael Reid <m.reid@sportradar.com>
2019-06-29 07:01:19 +0000

    javadoc. code cleanup

--- src/auction/AuctionListener.java
+++ src/auction/AuctionListener.java
@@ -1,5 +1,13 @@
 package auction;
 
+/**
+ * A simple callback interface to allow for a hook into the bidding process
+ */
 public interface AuctionListener {
+    /**
+     * Reports back with the values of the two bids made by the Bidders
+     * @param bid1 the bid made by bidder1
+     * @param bid2 the bid made by bidder2
+     */
     void onBidsRevealed(int bid1, int bid2);
 }
